b83bfdcaf108fd6705c247e62678383cc5bf2364
WillForan <willforan@gmail.com>
2018-01-05 11:56:56 -0500

    init

---
+++ readme.md
@@ -0,0 +1,2 @@
+# LNCD utilties for MRRC
+ - `waitForScannerTrigger.m` update matlab+PTB script to wait for scanner triggered fast simulated keypress
---
+++ waitForScannerTrigger.m
@@ -0,0 +1,38 @@
+function trgrtime = waitForScannerTrigger()
+   % 20180103 WF - wait for trigger keypess
+   %  use PTB's KbQueue for new Psychology Software Tools button box
+   %  because simulated keypress is too fast to be consistnatly captured by ListenChar/KbWait/KbCheck
+   %  trigger is a simulated "=" keypress
+
+   % what key(s) are acceptiable scanner start triggers
+   triggerKeys = {'=+'};
+
+   % setup: var, keys, and queue
+   scannerTrigger = 0;
+   KbName('UnifyKeyNames');
+   triggerIdx = KbName(triggerKeys);
+   % ListenChar(2); % we can tell matlab to not show keypresses
+   KbQueueCreate(); KbQueueStart();
+
+   % keep checking until the scanner says it started
+   while ~scannerTrigger
+     [pressed, firstPress, ~,~,~] = KbQueueCheck();
+     scannerTrigger = pressed && ...
+                     any(ismember(triggerKeys, KbName(firstPress)));
+   end
+
+   % grab the time of the button push. look at press of first (by key order) trigger key
+   [~,~, trgrtime ] = find(firstPress(triggerIdx),1)
+
+   % release queue -- unblock listening to keyboard -- so we can use KbWait later
+   % this takes about .090 seconds
+   % which is why we grab time above
+   KbQueueStop(); KbQueueRelease()
+   
+   % % previously trigger simulated "key press" was long enough to be captured by KbWait
+   % while ~ scannerTrigger
+   %   [~, keyCode, ~]  = KbWait;
+   %   scannerTrigger = any(ismember({'=','=+'}, KbName(keyCode)));
+   % end
+
+end     
