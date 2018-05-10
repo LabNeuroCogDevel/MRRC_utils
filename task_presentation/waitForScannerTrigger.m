d06033b304cb109d22a123170302461cf58b619c
WillForan <willforan@gmail.com>
2018-05-10 13:57:01 -0400

    update waitForScannerTrigger to take options re triggers kb_created, deviceNumber

--- task_presentation/waitForScannerTrigger.m
+++ task_presentation/waitForScannerTrigger.m
@@ -1,38 +1,71 @@
-function trgrtime = waitForScannerTrigger()
-   % 20180103 WF - wait for trigger keypess
-   %  use PTB's KbQueue for new Psychology Software Tools button box
-   %  because simulated keypress is too fast to be consistently captured by ListenChar/KbWait/KbCheck
-   %  trigger is a simulated "=" keypress
+% waitForScannerTrigger  -- block until keyboard gets a trigger from the scanner
+% uses KbQueue to wait for scanner trigger. could use KbQueueWait instead of this function
+% options:
+%    waitForScannerTrigger('triggerKeys',{'6^'}) -- set trigger to wait for to ^
+%    waitForScannerTrigger('kb_created',1)       -- already have a kbqueue, dont create one
+%    waitForScannerTrigger('deviceNumer',0)      -- set the device number
+%    "On MS-Windows it is not possible to enumerate different keyboards and mice
+%     separately. Therefore the 'deviceNumber' argument is mostly useless ..."
+function trgrtime = waitForScannerTrigger(varargin)
 
-   % what key(s) are acceptable scanner start triggers
-   triggerKeys = {'=+'};
+% 20190510 WF - task uses kbqueue, so do not create/release
+%  add options for 'trigger', 'kb_created', and deviceNumber
 
-   % setup: var, keys, and queue
-   scannerTrigger = 0;
-   KbName('UnifyKeyNames');
-   triggerIdx = KbName(triggerKeys);
-   % ListenChar(2); % we can tell matlab to not show keypresses
-   KbQueueCreate(); KbQueueStart();
+% 20180103 WF - initial
+%  use PTB's KbQueue for new Psychology Software Tools button box
+%  because simulated keypress is too fast to be consistently captured by ListenChar/KbWait/KbCheck
+%  trigger is a simulated "=" keypress
 
-   % keep checking until the scanner says it started
-   while ~scannerTrigger
-     [pressed, firstPress, ~,~,~] = KbQueueCheck();
-     scannerTrigger = pressed && ...
-                     any(ismember(triggerKeys, KbName(firstPress)));
+%% parse options
+% defaults
+opts.triggerKeys = {'=+'}; % what will trigger continue
+opts.deviceNumber= [];     % Device ID. More than likely never matters
+opts.kb_created  = 0;      % we haven't already run KbQueueCreate
+
+% parse any options
+i=1;
+opt_names = fieldnames(opts); % need to unnest
+while i<nargin
+   if contains(varargin{i},{opt_names{:}}) % 'contains' from R2016a
+      opts.(varargin{i}) = varargin{i+1};
+      i=i+1;
    end
+   i=i+1;
+end
 
-   % grab the time of the button push. look at press of first (by key order) trigger key
-   [~,~, trgrtime ] = find(firstPress(triggerIdx),1)
+ 
+%% setup: var, keys, and queue
+KbName('UnifyKeyNames');
+triggerIdx = KbName(opts.triggerKeys);
+%ListenChar(2); % we can tell matlab to not show keypresses
+% reported to work poorly with windows > xp
 
-   % release queue -- unblock listening to keyboard -- so we can use KbWait later
-   % this takes about .090 seconds
-   % which is why we grab time above
-   KbQueueStop(); KbQueueRelease()
-   
-   % % previously trigger simulated "key press" was long enough to be captured by KbWait
-   % while ~ scannerTrigger
-   %   [~, keyCode, ~]  = KbWait;
-   %   scannerTrigger = any(ismember({'=','=+'}, KbName(keyCode)));
-   % end
+fprintf('=== WAITING FOR TRIGGER ===\n')
 
-end     
+%% start queue
+% only create a queue if we aren't told one already exists
+if ~opts.kb_created; KbQueueCreate(opts.deviceNumber); end
+KbQueueStart(opts.deviceNumber);
+
+%% wait for trigger
+% keep checking until the scanner says it started, essentially reimplement 'KbQueueWait'
+scannerTrigger = 0;
+while ~scannerTrigger
+    [pressed, firstPress, ~,~,~] = KbQueueCheck(opts.deviceNumber);
+    scannerTrigger = pressed && ...
+        any(ismember(opts.triggerKeys, KbName(firstPress)));
+end
+ 
+% grab the time of the button push. look at press of first (by key orsca
+[~,~, trgrtime ] = find(firstPress(triggerIdx),1);
+ 
+
+%% clean up
+KbQueueStop(opts.deviceNumber);
+KbQueueFlush(opts.deviceNumber);
+% releasing queue so we can use eg. KbWait
+if ~opts.kb_created; KbQueueRelease(opts.deviceNumber); end
+%  only do if we didn't have a queue before starting this function
+% costs 90ms.
+ 
+end
